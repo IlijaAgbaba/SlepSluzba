2ede58793cc7e7f83cf631e033468f778458b0dc
IlijaAgbaba <36776221+IlijaAgbaba@users.noreply.github.com>
2022-09-11 19:00:50 +0200

    Dodavanje kontrolera za komentare

---
+++ RsokProject/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using RsokProject.DataLayer;
+using RsokProject.ViewModel;
+using RsokProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RsokProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Listanje svih komentara
+            Baza baza = new Baza();
+            List<Komentar> sviKomentari = baza.sviKomentari;
+
+            return View(sviKomentari);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string tekstKomentara)
+        {
+            //Čuvanje imena, teksta komentara i datum
+            var ident = System.Web.HttpContext.Current.User.Identity.Name;
+            Komentar komentar = new Komentar();
+            komentar.ime = ident.ToString();
+            komentar.tekstKomentara = tekstKomentara;
+            komentar.datum = DateTime.Now;
+
+            //Upisivanje komentara u bazu
+            Baza baza = new Baza();
+            baza.upisiKomentar(komentar);
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
